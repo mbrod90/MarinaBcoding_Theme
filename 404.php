a2045a86963ca916da7bf1224d72e1f34109310d
Marina Brodskaya <mbrod90@gmail.com>
2017-11-18 15:19:21 -0500

    init theme files

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section class="row">
+  <div class="small-12 columns text-center">
+    <div class="leader">
+        <p><?php _e( 'Sorry, no pages found.', 'marinabcoding-com-portfolio-theme'); ?></p>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
